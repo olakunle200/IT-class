f8cd6dc16e67903dba3cda2746e88800ba8f661c
kulkidk <olakunlemicah@gmail.com>
2021-05-23 13:51:17 -0700

    new commitonjs

---
+++ AssignmentOnjs.js
@@ -0,0 +1,17 @@
+let num1 = 20;
+let num2 = 10;
+if(num1 >= num2){
+    console.log("number1 is greater")
+} else{
+    console.log("number2 is lesser")
+}
+
+var age1 = 21;
+var age2 = 13;
+if(age1 == 18 || age1 > 18 ){
+    console.log("CONGRATULATION YOUR ARE ADMITTED")
+}else if(age2 == 18 && age2 <13){
+    console.log("SORRY YOU ARE NOT ADMITTED")
+}else{
+    console.log("PLEASE STAY AT HOME")
+}
---
+++ class2onjs.js
@@ -0,0 +1,19 @@
+//console.log(5 === 6);
+var age = 10;
+var gender = "m";
+//if(age >=18 && gender == "m"){
+  //  console.log("You can play because you are  " +age );
+
+//} else{
+   // console.log("play at home");
+//}
+  
+//}
+
+if(age >=18 && gender == "m"){
+      console.log("You can play PS5 cos you are  " +age+ "years old" );
+}else if(age < 18 && age > 10){
+    console.log("you can play PS5 cos you are" +age+"years old");
+} else{
+    console.log("you can go home and play your game");
+}
